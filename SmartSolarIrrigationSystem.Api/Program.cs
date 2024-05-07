785dc6034422b3a7f1fad12a57bc51b9fe74a8ec
nunnasir <nunnasir@gmail.com>
2024-05-07 21:07:10 +0600

    Update Cors Policy

--- SmartSolarIrrigationSystem.Api/Program.cs
+++ SmartSolarIrrigationSystem.Api/Program.cs
@@ -1,7 +1,7 @@
-using SmartSolarIrrigationSystem.Mqtt;
 using SmartSolarIrrigationSystem.Application;
-using SmartSolarIrrigationSystem.Mqtt.Models;
 using SmartSolarIrrigationSystem.Application.Database;
+using SmartSolarIrrigationSystem.Mqtt;
+using SmartSolarIrrigationSystem.Mqtt.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -13,19 +13,31 @@ builder.Services.Configure<MqttSettings>(config.GetSection("MqttSettings"));
 
 builder.Services.AddMqtt();
 builder.Services.AddApplication();
-
 builder.Services.AddDatabase(config["Database:ConnectionString"]!);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder =>
+        {
+            builder.WithOrigins("https://project-dashboard-roan.vercel.app")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors("AllowOrigin");
+
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.UseCors();
 
 var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
 await dbInitializer.InitializeAsync();
